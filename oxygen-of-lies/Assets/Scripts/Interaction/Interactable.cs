a027ea3601a0b4b838c05e8319a54c3013f1f07b
Emir Beşir <emirbesirx@gmail.com>
2026-01-31 23:28:11 +0300

    feat: Implement unified interaction system and game flags management

--- oxygen-of-lies/Assets/Scripts/Interaction/Interactable.cs
+++ oxygen-of-lies/Assets/Scripts/Interaction/Interactable.cs
@@ -1,20 +1,56 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Interactable : MonoBehaviour
+/// <summary>
+/// Simple examinable object - press E to see description.
+/// Implements IInteractable for unified interaction system.
+/// </summary>
+public class Interactable : MonoBehaviour, IInteractable
 {
     [Header("Descriptions")]
     [SerializeField][TextArea(2, 5)] private string _maskedDescription = "A beautiful vase.";
     [SerializeField][TextArea(2, 5)] private string _realDescription = "It's... a skull.";
     
-    [Header("Settings")]
-    [SerializeField] private float _interactionDistance = 3f;
+    [Header("Prompts")]
+    [SerializeField] private string _defaultPrompt = "E - İncele";
     
-    public string MaskedDescription => _maskedDescription;
-    public string RealDescription => _realDescription;
-    public float InteractionDistance => _interactionDistance;
+    [Header("Events")]
+    public UnityEvent OnExamined;
     
-    public string GetDescription(bool isMaskOn)
+    private bool _showingDescription = false;
+    
+    // IInteractable implementation
+    public bool RequiresHold => false;
+    public float HoldDuration => 0f;
+    
+    public string GetPrompt(bool isMaskOn)
     {
-        return isMaskOn ? _maskedDescription : _realDescription;
+        if (_showingDescription)
+        {
+            return isMaskOn ? _maskedDescription : _realDescription;
+        }
+        return _defaultPrompt;
+    }
+    
+    public bool CanInteract(bool isMaskOn)
+    {
+        return true;
+    }
+    
+    public void OnInteract()
+    {
+        _showingDescription = true;
+        OnExamined?.Invoke();
+        Debug.Log($"[Interactable] Showing description");
+    }
+    
+    public void OnHoldProgress(float progress) { }
+    public void OnHoldComplete() { }
+    
+    // Reset when player looks away (called by controller)
+    public void ResetState()
+    {
+        _showingDescription = false;
     }
 }
+
